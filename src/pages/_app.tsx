725f67bca019359628ff9cd2fb3c95e0f5b7e05d
maiptn <mai.pham@on.net.vn>
2023-02-01 18:13:59 +0700

    fix favicon not showing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,7 +18,7 @@ const App = ({ Component, pageProps }) => {
           key="viewport"
           maximum-scale="1"
         />
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico?" />
       </Head>
 
       <div
